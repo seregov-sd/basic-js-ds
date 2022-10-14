e59ee16b47b45ffbe8373870ecea9c86cae4b4db
seregov-sd <seregov.sd@yandex.by>
2022-10-14 17:12:13 +0300

    feat: completed the task stack

--- src/stack.js
+++ src/stack.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement the Stack with a given interface via array.
@@ -13,22 +13,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor() {
+        this.stack = [];
+    }
 
-  peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    push(element) {
+        this.stack.unshift(element);
+    }
+
+    pop() {
+        return this.stack.shift();
+    }
+
+    peek() {
+        return this.stack[0];
+    }
 }
 
 module.exports = {
-  Stack
+    Stack
 };
